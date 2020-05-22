cc0be908ecdd163d0a0af14ee5de5d4196af55f9
Csaba Salat <csalat@globalblue.com>
2020-05-22 09:58:40 +0200

    init

---
+++ src/Infra.Sqlite.InMemoryDb/ServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NetExtensions.Infra.Sqlite.InMemoryDb
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddSqliteInMemoryDb<TContext>(this IServiceCollection services) where TContext : DbContext, new()
+        {
+            var connection = new SqliteConnection("DataSource=:memory:");
+            connection.Open();
+            services.AddSingleton(sp => connection);
+            var options = new DbContextOptionsBuilder<TContext>()
+                .UseSqlite(connection)
+                .Options;
+            using var context = CreateContext(options);
+            context.Database.EnsureCreated();
+
+            services.AddSingleton(sp => options);
+            return services;
+        }
+
+        private static TContext CreateContext<TContext>(DbContextOptions<TContext> options) where TContext : DbContext, new() => (TContext)Activator.CreateInstance(typeof(TContext), options);
+    }
+}
